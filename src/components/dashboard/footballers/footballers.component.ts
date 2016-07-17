cb4d7bfcd387f474c52c06d8ff779c43e4a571c6
Damian Bachórz <damian.bachorz@briisk.co>
2016-07-17 16:12:30 +0200

    style fix

--- src/components/dashboard/footballers/footballers.component.ts
+++ src/components/dashboard/footballers/footballers.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component ({
   selector: 'footballers',
   template: require('./footballers.component.html'),
-  styleUrls: ['/footballers.component.less']
+  styles: [require('./footballers.component.less')]
 })
 export class FootballersComponent {
   constructor() {
