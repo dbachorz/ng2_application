3edb21aad1d53d20216d79d8a30d8bf5f031953c
Damian Bachórz <damian.bachorz@briisk.co>
2016-09-05 23:34:14 +0200

    fetching from api in edit view

--- src/components/edit-footballer/edit-footballer.component.ts
+++ src/components/edit-footballer/edit-footballer.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { FootballersService } from '../../services/footballers.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -8,17 +8,44 @@ import { ActivatedRoute } from '@angular/router';
   template: require('./edit-footballer.component.html'),
   styles: [require('./edit-footballer.component.scss')],
 })
-export class EditFootballerComponent implements OnInit {
-  private footballerId: string;
+export class EditFootballerComponent {
   private myForm: FormGroup;
   private firstName: FormControl;
   private lastName: FormControl;
   private country: FormControl;
   private club: FormControl;
 
+  private footballerId: string;
+
   constructor(private footballersService: FootballersService,
-              private activatedRoute: ActivatedRoute) { }
-  ngOnInit() {
-    this.activatedRoute.params.subscribe((params: any) => this.footballerId = params.footballer_id);
+              private activatedRoute: ActivatedRoute,
+              private builder: FormBuilder) {
+
+    this.firstName = new FormControl('');
+    this.lastName = new FormControl('');
+    this.country = new FormControl('');
+    this.club = new FormControl('');
+
+    this.myForm = this.builder.group(
+      Object.keys(this)
+        .filter((attr) => this[attr] instanceof FormControl)
+        .reduce((accumulator, currentVal) => {
+          accumulator[currentVal] = this[currentVal];
+          return accumulator;
+        }, {})
+    );
+
+    this.activatedRoute.params
+      .subscribe((params: any) => this.footballerId = params.footballer_id);
+
+    this.footballersService.fetchFootballer(this.footballerId)
+      .filter(res => !!res)
+      .subscribe((product: any) => {
+        Object.keys(product)
+          .filter((key) => product.hasOwnProperty(key) && this.myForm.controls[key] instanceof FormControl)
+          .forEach((key) => {
+            (<FormControl>this[key]).updateValue(product[key]);
+          });
+      });
   }
 }
